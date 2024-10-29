11ba0629c42abfc5583720facdfdb158409a826d
Firdous Ali <firdous.ali86@gmail.com>
2024-10-29 17:09:19 +0500

    ios native module

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TestNativeModule
+//
+//  Created by Apple on 29/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
